4e7f76c32e96ddeab690081349085e72257edd29
um1d0 <245683239+um1d0@users.noreply.github.com>
2026-05-16 23:57:42 +0400

    aaa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import {apikeyinterceptor} from './api-key-interceptor';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
